bf2216c56baa50be67a6ad7f0e3d6833f1c94357
= <miguelisaac1985@hotmail.com>
2016-04-07 15:46:51 -0300

    This closes #11

--- models/mEquipos.js
+++ models/mEquipos.js
@@ -5,5 +5,5 @@ module.exports = {
 }
 
 function getAll(cb) {
-	conn("select * from equipos",cb);
+	conn("select id, numero, denominacion, numero_coche_fk, DATE_FORMAT(fecha_colocacion, '%d/%m/%Y') as f_colocacion, total, id_operador_fk, responsable, tipo, DATE_FORMAT(fecha_sacado, '%d/%m/%Y') as f_sacado, km, resultado from equipos",cb);
 }
