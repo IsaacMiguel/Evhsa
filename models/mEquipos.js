a5e7bdafc33d76022e69f3b1cf81774a59a4ba19
= <miguelisaac1985@hotmail.com>
2016-04-08 18:36:11 -0300

    Adding querys readById, update and delete equipos

--- models/mEquipos.js
+++ models/mEquipos.js
@@ -2,14 +2,17 @@ var conn = require('../config/db').conn;
 
 module.exports = {
 	getAll : getAll,
-	insert : insert
+	insert : insert,
+	getById : getById,
+	updateEquipo : updateEquipo,
+	delEquipo : delEquipo
 }
 
-function getAll(cb) {
+function getAll (cb) {
 	conn("select id, numero, denominacion, numero_coche_fk, DATE_FORMAT(fecha_colocacion, '%d/%m/%Y') as f_colocacion, total, responsable, tipo, DATE_FORMAT(fecha_sacado, '%d/%m/%Y') as f_sacado, km, resultado from equipos",cb);
 }
 
-function insert(d, cb) {
+function insert (d, cb) {
 	conn(
 		"insert into equipos" +
 		"(numero,denominacion,numero_coche_fk,fecha_colocacion,total,unica_operador_fk,responsable," +
@@ -17,4 +20,29 @@ function insert(d, cb) {
 		"values("+d.numero+",'"+d.denominacion+"',"+d.numero_coche_fk+",'"+
 		d.fecha_colocacion+"',"+d.total+","+d.unica_operador_fk+",'"+d.responsable+"','"+
 		d.observaciones+"','"+d.tipo+"','"+d.fecha_sacado+"',"+d.km+",'"+d.resultado+"')", cb);
+}
+
+function getById (id, cb) {
+	conn("select * from equipos where id=" + id, cb);
+}
+
+function updateEquipo (d, cb) {
+	conn("update equipos set" +
+		" numero=" + d.numero +
+		", denominacion='" + d.denominacion + "'" +
+		", numero_coche_fk=" + d.numero_coche_fk +
+		", fecha_colocacion='" + d.fecha_colocacion + "'" +
+		", total=" + d.total +
+		", unica_operador_fk=" + d.unica_operador_fk +
+		", responsable='" + d.responsable + "'" +
+		", observaciones='" + d.observaciones + "'" +
+		", tipo='" + d.tipo + "'" +
+		", fecha_sacado='" + d.fecha_sacado + "'" +
+		", km=" + d.km +
+		", resultado='" + d.resultado + "'" +
+	" where id=" + d.id, cb);
+}
+
+function delEquipo (id, cb) {
+	conn("delete from equipos where id=" + id, cb);
 }
