b57f796f0cf29abc7c319246ec6e0b6f32ce2c63
= <miguelisaac1985@hotmail.com>
2016-04-15 18:32:07 -0300

    Merge...

--- public/js/main.js
+++ public/js/main.js
@@ -180,5 +180,4 @@ function generateTodayDate(){
 function maxLengthCheck(object) {
     if (object.value.length > object.maxLength)
       object.value = object.value.slice(0, object.maxLength)
-
 }
