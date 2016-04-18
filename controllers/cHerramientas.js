89831471114c725b308bf71d2cb0a5a538c16b3e
= <miguelisaac1985@hotmail.com>
2016-04-18 20:08:41 -0300

    Adding tasks to #18 Milestone -Modulo Herramientas-

--- controllers/cHerramientas.js
+++ controllers/cHerramientas.js
@@ -1,31 +1,23 @@
 var mHerramientas = require('../models/mHerramientas');
+var mRepuestos = require('../models/mRepuestos');
 
 module.exports = {
 	getLista : getLista,
-	getFiltrar : getFiltrar
+	getFiltrar : getFiltrar,
+	getAlta : getAlta,
+	getRepuestos : getRepuestos
 }
 
-function getLista(req, res) {
+function getLista (req, res) {
 	res.render('herramientas_lista', {
-		pagename : 'Lista de Herramientras'
+		pagename : 'Lista de Herramientas'
 	});
 }
 
 function getFiltrar (req, res) {
 	var params = req.params;
 
-	if (params.toolName === 'NoName') {
-		var data = {
-			'date1' : params.date1, 
-			'date2' : params.date2, 
-			'toolname' : params.toolName
-		}
-
-		mHerramientas.getByFecha_Nombre(data, function (herramientas) {
-			res.send(herramientas);
-		});
-
-	} else {
+	if (params.denominacion === 'NoName') {
 		var data = {
 			'date1' : params.date1, 
 			'date2' : params.date2
@@ -33,6 +25,37 @@ function getFiltrar (req, res) {
 
 		mHerramientas.getByFecha(data, function (herramientas) {
 			res.send(herramientas);
+		});
+
+	} else {
+		var data = {
+			'date1' : params.date1, 
+			'date2' : params.date2, 
+			'denominacion' : params.denominacion
+		}
+
+		mHerramientas.getByFecha_Nombre(data, function (herramientas) {
+			res.send(herramientas);
+		});
+	}
+}
+
+function getAlta (req, res) {
+	res.render('herramientas_alta_filtro', {
+		pagename : 'Filtro de alta de Herramientas'
+	});
+}
+
+function getRepuestos (req, res) {
+	var params = req.params;
+
+	if (params.codigo !== false) {
+		mRepuestos.getByCodigo(params.codigo, function (repuestos) {
+			res.send(repuestos);
+		});
+	} else {
+		mRepuestos.getByDescripcion(params.descripcion, function (repuestos) {
+			res.send(repuestos);
 		});
 	}
 }
