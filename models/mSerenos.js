fe33bc18c7606b85d129738dc12fdde3b97f6754
= <miguelisaac1985@hotmail.com>
2016-05-13 17:10:02 -0300

    Adding alias for displayed DB field

--- models/mSerenos.js
+++ models/mSerenos.js
@@ -19,7 +19,8 @@ function getAllHerramientas (fecha_desde, fecha_hasta, cb) {
 	conn("SELECT *, DATE_FORMAT(fecha_movimiento, '%d/%m/%Y') as fecha_movimiento_f, " +
 	"DATE_FORMAT(fecha_colocado, '%d/%m/%Y') as fecha_colocado_f, " +
 	"repuestos.valor AS valor_unitario, " +
-	"serenos.id as id_serenos " +
+	"serenos.id as id_serenos, " +
+	"serenos.cantidad as serenocantidad " +
 	"FROM `serenos` " +
 	"LEFT JOIN repuestos ON serenos.id_repuesto_fk = repuestos.id " +
 	"LEFT JOIN vehiculos ON serenos.numero_coche = vehiculos.id " +
@@ -31,7 +32,8 @@ function getHerramientasByColocados (fecha_desde, fecha_hasta, cb) {
 	conn("SELECT *, DATE_FORMAT(fecha_movimiento, '%d/%m/%Y') as fecha_movimiento_f, " +
 	"DATE_FORMAT(fecha_colocado, '%d/%m/%Y') as fecha_colocado_f, " +
 	"repuestos.valor AS valor_unitario, " +
-	"serenos.id as id_serenos " +
+	"serenos.id as id_serenos, " +
+	"serenos.cantidad as serenocantidad " +
 	"FROM `serenos` " +
 	"LEFT JOIN repuestos ON serenos.id_repuesto_fk = repuestos.id " +
 	"LEFT JOIN vehiculos ON serenos.numero_coche = vehiculos.id " +
@@ -44,7 +46,8 @@ function getHerramientasByNoColocados (fecha_desde, fecha_hasta, cb) {
 	conn("SELECT *, DATE_FORMAT(fecha_movimiento, '%d/%m/%Y') as fecha_movimiento_f, " +
 	"DATE_FORMAT(fecha_colocado, '%d/%m/%Y') as fecha_colocado_f, " +
 	"repuestos.valor AS valor_unitario, " +
-	"serenos.id as id_serenos " +
+	"serenos.id as id_serenos, " +
+	"serenos.cantidad as serenocantidad " +
 	"FROM `serenos` " +
 	"LEFT JOIN repuestos ON serenos.id_repuesto_fk = repuestos.id " +
 	"LEFT JOIN vehiculos ON serenos.numero_coche = vehiculos.id " +
